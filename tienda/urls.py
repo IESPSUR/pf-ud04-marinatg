ce43b409f803e7f59394ff5e4bd9481138a25287
Alumnado <marina.teran.gallardo.al@iespoligonosur.org>
2022-11-11 10:27:54 +0100

    venv y bd

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('tienda/', views.welcome, name='welcome'),
